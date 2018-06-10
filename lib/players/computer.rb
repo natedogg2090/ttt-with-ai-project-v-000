5c94319bac4a4646881dd1791cc4fcb8077a44c9
Nathan Dietz <nsdietz@gmail.com>
2018-06-10 11:36:07 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,12 @@ module Players
 
   class Computer < Player
     def move(board)
-      input = "1"
+      array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      input = array.find {|input| board.valid_move?(input)}
+
+      # if board.valid_move?(input) # if this is true, return the value of input
+      #   input = "1"
+      # end
     end
   end
 
