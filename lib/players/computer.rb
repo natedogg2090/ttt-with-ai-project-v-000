e30a3ed899717aa32e5d9bbdf65ae080380adb19
Nathan Dietz <nsdietz@gmail.com>
2018-06-10 12:13:35 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,7 @@ module Players
     def move(board)
       array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       binding.pry
-      input = array.find {|position| valid_move?(position)}
+      input = array.find {|position| board.valid_move?(position)}
     end
   end
 
